2f7c8a33188b7a947e228521ad62a32fd40455c0
黑高粱米 <454082559@qq.com>
2015-05-04 10:52:56 +0800

    AspNetPager
    支持Route，增加上一页下一页单独CSS样式名

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Web.UI;
+using System.Security;
+
+[assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level1)]
+[assembly: AssemblyTitle("AspNetPager 7.4.5")]
+[assembly: AssemblyDescription("ASP.NET·ÖÒ³¿Ø¼þ")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetPager")]
+[assembly: AssemblyCopyright("Webdiyer")]
+[assembly: AssemblyTrademark("Webdiyer")]
+[assembly: AssemblyCulture("")]		
+[assembly: TagPrefix("Wuqi.Webdiyer","webdiyer")]
+[assembly: AssemblyVersion("7.4.5")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyName("")]
+[assembly: AllowPartiallyTrustedCallers()]
---
+++ README.md
@@ -0,0 +1,2 @@
+# AspNetPager
+支持Route，增加上一页下一页CSS
